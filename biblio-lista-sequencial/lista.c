762834d29a655614d05403acbafecf8ac5a19996
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-15 13:04:30 -0400

    arquivo com as funções

---
+++ biblio-lista-sequencial/lista.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+Lista criar(int tam){
+    Lista n = malloc(tam*sizeof(struct lista));
+    n->tamanho=tam;
+    n->total=0;
+    n->item=malloc(tam*sizeof (Item));
+    return (n);
+}
+
+
+Lista inserir(Item x,Lista p)
+{
+    if(p->total<p->tamanho){
+        p->item[p->total]=x;
+        p->total++;
+    }
+    else
+        printf("\nLista cheia!");
+    return p;
+}
+
+void exibir(Lista p)
+{
+    int i=0;
+    while(i<p->total)
+    {
+       printf("\nID: %d  ",p->item[i].id);
+       printf("Nome: %s",p->item[i].nome);
+       i++;
+    }
+}
