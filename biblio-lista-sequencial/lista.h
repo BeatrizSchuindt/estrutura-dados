305104246f74f0a7d8e66ccfc1715650b62ecc04
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-15 13:03:35 -0400

    arquivo com as declarações de funções criado

---
+++ biblio-lista-sequencial/lista.h
@@ -0,0 +1,26 @@
+//1º passo - definição do TAD
+//criação da struct Pessoa
+struct Coisa{
+    int id;
+    char *nome;
+};
+//Criação do tipo de dados Item que se baseaia em Pessoa
+typedef struct Coisa Item;
+
+//Criação do tipo da lista
+struct lista{
+    int total; 
+    int tamanho;
+    Item *item;
+};
+
+typedef struct lista *Lista;
+
+//funções de manipulação da lista
+Lista criar(int tam);
+Lista inserir(Item x,Lista p);
+void exibir(Lista l);
+//remoção inicio
+//remoção meio
+//remoção fim
+//busca/pertinência
