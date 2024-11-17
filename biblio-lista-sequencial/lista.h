394cb0ed76ecbbf276017961a2de67b753c27bc5
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-17 13:09:27 -0400

    função remover qualquer declarada

--- biblio-lista-sequencial/lista.h
+++ biblio-lista-sequencial/lista.h
@@ -21,7 +21,7 @@ Lista criar(int tam);
 Lista inserir(Item x,Lista p);
 void exibir(Lista l);
 int remover_inicio(Lista p);
-//remoção meio
+int remover_meio(Lista p, int id);//remoção meio
 int remover_final(Lista p);//remoção fim
 int buscar(Lista p, int id, Item *resultado);//busca/pertinência
 void destruir(Lista p);//destruição
