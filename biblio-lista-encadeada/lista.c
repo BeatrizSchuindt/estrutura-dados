f8b37f3d3d3b119c5bcd25e8ee95926eee9bec60
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-18 08:47:35 -0400

    função inserir ordenado implementada

--- biblio-lista-encadeada/lista.c
+++ biblio-lista-encadeada/lista.c
@@ -38,6 +38,27 @@ int inserir_final(Lista *p, Item x) {
     return 1;
 }
 
+int inserir_ordenado(Lista *p, Item x) {
+    No *novo = (No *)malloc(sizeof(No));
+    if (novo == NULL)
+        return 0;
+
+    novo->item = x;
+    if (*p == NULL || (*p)->item.id > x.id) {
+        novo->prox = *p;
+        *p = novo;
+    } else {
+        No *ant = NULL, *atual = *p;
+        while (atual != NULL && atual->item.id < x.id) {
+            ant = atual;
+            atual = atual->prox;
+        }
+        novo->prox = atual;
+        ant->prox = novo;
+    }
+    return 1;
+}
+
 void exibir_lista(Lista p) {
     No *aux = p;
     while (aux != NULL) {
