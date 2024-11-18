afe906dac4a2547e7548fad75f5da88dc6411184
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-18 16:31:32 -0400

    reformulação do ponteiro na criação da lista

--- biblio-lista-encadeada/lista.h
+++ biblio-lista-encadeada/lista.h
@@ -14,17 +14,16 @@ typedef struct no {
 //apontador para o primeiro nó da lista
 typedef No *Lista;
 
-Lista criar_lista();//criar
-int inserir_inicio(Lista *p, Item x);//inserir no início
-int inserir_final(Lista *p, Item x);//inserir no final
-int inserir_ordenado(Lista *p, Item x);//inserir de forma ordenada
-void exibir_lista(Lista p);//exibir elementos
-int remover_inicio(Lista *p);//remover do início
-int remover_final(Lista *p);//remover do final
-int remover_meio(Lista *p, int id);//remover do meio
-int tamanho_lista(Lista p);//tamanho da lista
-int buscar(Lista p, int id, Item *resultado);//buscar elemento
-void destruir_lista(Lista *p);//destruir a lista
+Lista* criar_lista(); // Criar lista
+int inserir_inicio(Lista* p, Item x); //inserir no início
+int inserir_final(Lista* p, Item x); //inserir no final
+int inserir_ordenado(Lista* p, Item x); //inserir de forma ordenada
+void exibir_lista(Lista p); //exibir elementos
+int remover_inicio(Lista* p); //remover do início
+int remover_final(Lista* p); //remover do final
+int remover_meio(Lista* p, int id); //remover do meio
+int tamanho_lista(Lista p); //tamanho da lista
+int buscar(Lista p, int id, Item *resultado); //buscar elemento
+void destruir_lista(Lista* p); //destruir a lista
 
-//funçõezinhas 'bônus'
-int lista_vazia(Lista p);
+int lista_vazia(Lista p); //verifica se a lista está vazia
