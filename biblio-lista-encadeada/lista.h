6ec937dd0a067a54df013abac318cdc5d2ba3cd9
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2024-11-18 09:34:12 -0400

    função remover final declarada

--- biblio-lista-encadeada/lista.h
+++ biblio-lista-encadeada/lista.h
@@ -20,7 +20,7 @@ int inserir_final(Lista *p, Item x);//inserir no final
 int inserir_ordenado(Lista *p, Item x);//inserir de forma ordenada
 void exibir_lista(Lista p);//exibir elementos
 int remover_inicio(Lista *p);//remover do início
-//remover do final
+int remover_final(Lista *p);//remover do final
 //remover do meio
 int buscar(Lista p, int id, Item *resultado);//buscar elemento
 void destruir_lista(Lista *p);//destruir a lista
