eeb1962c5291a2f7494b59635830bd4e1b156633
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2025-02-04 14:25:58 -0400

    implementação das funções que manipulam as expressões numéricas

--- prova3/expressao.c
+++ prova3/expressao.c
@@ -4,3 +4,144 @@
     Nome: Ana Beatriz Schuindt do Amaral e Matheus Henrique Moreira Louro
     Data: 15/02/25
 *******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "pilha-char.h"
+#include "pilha-float.h"
+#include "expressao.h"
+
+typedef struct {
+    char nome;
+    float valor;
+} Variavel;
+
+Variavel variaveis[26];
+int numVariaveis = 0;
+
+// função para definir a prioridade dos operadores
+int prioridade(char operador) {
+    switch (operador) {
+        case '+': case '-': return 1;
+        case '*': case '/': return 2;
+        default: return 0;  
+    }
+}
+
+// função para buscar o valor de uma variável
+float obterValorVariavel(char nome) {
+    for (int i = 0; i < numVariaveis; i++) {
+        if (variaveis[i].nome == nome) {
+            return variaveis[i].valor;
+        }
+    }
+    printf("Erro: Variável %c não encontrada!\n", nome);
+    exit(1);
+}
+
+// função para converter expressão infixa para pós-fixa
+void infixaParaPosfixa(const char *infixa, char *posfixa) {
+    PilhaC pilha = pilhaC(strlen(infixa));
+    int i, j = 0;
+
+    for (i = 0; infixa[i] != '\0'; i++) {
+        char c = infixa[i];
+
+        if (isalpha(c)) {
+            posfixa[j++] = c;
+        } else if (c == '(') {
+            empilhaC(c, pilha);
+        } else if (c == ')') {
+            while (!vaziapC(pilha) && topoC(pilha) != '(') {
+                posfixa[j++] = desempilhaC(pilha);
+            }
+            desempilhaC(pilha);
+        } else {
+            while (!vaziapC(pilha) && prioridade(topoC(pilha)) >= prioridade(c) && topoC(pilha) != '(') {
+                posfixa[j++] = desempilhaC(pilha);
+            }
+            empilhaC(c, pilha);
+        }
+    }
+
+    while (!vaziapC(pilha)) {
+        char op = desempilhaC(pilha);
+        if (op != '(') {
+            posfixa[j++] = op;
+        }
+    }
+
+    posfixa[j] = '\0';
+    destruir_pilhaC(&pilha);
+}
+
+// função para definir valores das variáveis
+void definirValoresVariaveis(const char *posfixa) {
+    numVariaveis = 0;
+    
+    for (int i = 0; posfixa[i] != '\0'; i++) {
+        char c = posfixa[i];
+        
+        if (isalpha(c)) {
+            int existe = 0;
+            for (int j = 0; j < numVariaveis; j++) {
+                if (variaveis[j].nome == c) {
+                    existe = 1;
+                    break;
+                }
+            }
+            if (!existe) {
+                variaveis[numVariaveis].nome = c;
+                numVariaveis++;
+            }
+        }
+    }
+
+    printf("\nDefina os valores das variáveis:\n");
+    for (int i = 0; i < numVariaveis; i++) {
+        printf("%c = ", variaveis[i].nome);
+        scanf("%f", &variaveis[i].valor);
+    }
+
+    printf("\nValores armazenados:\n");
+    for (int i = 0; i < numVariaveis; i++) {
+        printf("%c = %.2f\n", variaveis[i].nome, variaveis[i].valor);
+    }
+}
+
+// função para avaliar a expressão pós-fixa
+float avaliarExpressao(const char *posfixa) {
+    PilhaF pilha = pilhaF(strlen(posfixa));
+
+    for (int i = 0; posfixa[i] != '\0'; i++) {
+        char c = posfixa[i];
+
+        if (isalpha(c)) {
+            float valor = obterValorVariavel(c);
+            empilhaF(valor, pilha);
+        } else {
+            float op2 = desempilhaF(pilha);
+            float op1 = desempilhaF(pilha);
+            float resultado = 0;
+
+            switch (c) {
+                case '+': resultado = op1 + op2; break;
+                case '-': resultado = op1 - op2; break;
+                case '*': resultado = op1 * op2; break;
+                case '/':
+                    if (op2 == 0) {
+                        printf("Erro: Divisão por zero!\n");
+                        exit(1);
+                    }
+                    resultado = op1 / op2;
+                    break;
+            }
+            empilhaF(resultado, pilha);
+        }
+    }
+
+    float resultadoFinal = desempilhaF(pilha);
+    destruir_pilhaF(&pilha);
+    return resultadoFinal;
+}
